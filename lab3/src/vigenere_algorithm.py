71e8dc9a3675a92a17a21c1807af6e965eacdcf1
macriidanu <macriidani@.gmail.com>
2024-11-04 19:46:17 +0200

    report lab3 done

--- lab3/src/vigenere_algorithm.py
+++ lab3/src/vigenere_algorithm.py
@@ -13,12 +13,7 @@ def encrypt_vigenere(msg, key):
     key = generate_key(msg, key)
     for i in range(len(msg)):
         char = msg[i]
-        if char.isupper():
-            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
-        elif char.islower():
-            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
-        else:
-            encrypted_char = char
+        encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
         encrypted_text.append(encrypted_char)
     return "".join(encrypted_text)
 
@@ -27,11 +22,6 @@ def decrypt_vigenere(msg, key):
     key = generate_key(msg, key)
     for i in range(len(msg)):
         char = msg[i]
-        if char.isupper():
-            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
-        elif char.islower():
-            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
-        else:
-            decrypted_char = char
+        decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
         decrypted_text.append(decrypted_char)
     return "".join(decrypted_text)
